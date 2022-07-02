8e8f4411214f3ad1f4d929596ec9f14797d4522e
rhee <349249680@qq.com>
2022-07-02 17:23:48 +0800

    build project

---
+++ message-push-web/src/main/java/com/rhee/messagepush/MessagePushWebApplication.java
@@ -0,0 +1,13 @@
+package com.rhee.messagepush;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessagePushWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessagePushWebApplication.class, args);
+	}
+
+}
---
+++ message-push-web/src/test/java/com/rhee/messagepush/MessagePushWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rhee.messagepush;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessagePushWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
