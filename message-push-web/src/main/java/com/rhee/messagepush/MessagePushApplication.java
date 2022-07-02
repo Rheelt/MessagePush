b73fb838cc946932d14c2bfbf7a35be20b0575f6
rhee <349249680@qq.com>
2022-07-02 22:01:13 +0800

    build project

---
+++ message-push-web/src/main/java/com/rhee/messagepush/MessagePushApplication.java
@@ -0,0 +1,22 @@
+package com.rhee.messagepush;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author rhee
+ * @date 2022/7/2 9:57 PM
+ */
+
+@SpringBootApplication
+public class MessagePushApplication {
+    @RequestMapping("/")
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(MessagePushApplication.class, args);
+    }
+}
