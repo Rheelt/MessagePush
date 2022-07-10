4335476e66e0eee2feb556070f08c8deaa3f945b
rhee <349249680@qq.com>
2022-07-10 21:05:49 +0800

    sms demo

--- message-push-web/src/main/java/com/rhee/messagepush/MessagePushApplication.java
+++ message-push-web/src/main/java/com/rhee/messagepush/MessagePushApplication.java
@@ -4,9 +4,7 @@ import com.rhee.messagepush.script.SmsParam;
 import com.rhee.messagepush.script.TencentSmsScript;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
